9238ab8ec9c30a33bd12a747fe2802208732b346
JP Bacquet <jeanpolbac@gmail.com>
2019-10-10 15:39:28 +0000

    added api, added controller, started styling css.

--- app/controllers/conversions_controller.rb
+++ app/controllers/conversions_controller.rb
@@ -1,6 +1,11 @@
 class ConversionsController < ApplicationController
   def index
-
+    
   end
-  
+  def convert
+      result = params[:payload]
+      c = Converter.new
+      result[:amount] = c.get_conversion(result[:amount].to_f,result[:from_ticker],result[:to_ticker])
+      render :json => result
+  end
 end
